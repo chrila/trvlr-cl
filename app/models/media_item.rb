1491edbbcdcf3137d2905c8979f86b6f1baf4ade
Christian Lampl <christian.lampl@gmx.at>
2021-04-17 11:01:01 -0400

    Add: model MediaItem

---
+++ app/models/media_item.rb
@@ -0,0 +1,3 @@
+class MediaItem < ApplicationRecord
+  belongs_to :post
+end
---
+++ db/migrate/20210417150030_create_media_items.rb
@@ -0,0 +1,11 @@
+class CreateMediaItems < ActiveRecord::Migration[6.1]
+  def change
+    create_table :media_items do |t|
+      t.belongs_to :post, null: false, foreign_key: true
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
