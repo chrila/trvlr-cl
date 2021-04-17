26f191a0ede59fc1005b2fef415aab4cc0552cb5
Christian Lampl <christian.lampl@gmx.at>
2021-04-17 11:57:36 -0400

    Fix: changed interfering enum values

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -3,5 +3,5 @@ class Segment < ApplicationRecord
   belongs_to :waypoint_from, class_name: 'Waypoint'
   belongs_to :waypoint_to, class_name: 'Waypoint'
 
-  enum status: %i[open active finished]
+  enum status: %i[segment_open segment_active segment_finished]
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,6 @@ class Trip < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
 
-  enum status: %i[draft active finished cancelled]
-  enum visibility: %i[private users_only public]
+  enum status: %i[trip_draft trip_active trip_finished trip_cancelled]
+  enum visibility: %i[visibility_private visibility_users_only visibility_public]
 end
