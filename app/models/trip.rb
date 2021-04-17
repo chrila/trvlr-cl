5740419b2b86d9ed7e4dd23d45bffeafe0110c1c
Christian Lampl <christian.lampl@gmx.at>
2021-04-17 12:44:39 -0400

    Add: Waypoint relation to Trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,6 +1,7 @@
 class Trip < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
+  has_many :waypoints
 
   enum status: %i[trip_draft trip_active trip_finished trip_cancelled]
   enum visibility: %i[visibility_private visibility_users_only visibility_public]
