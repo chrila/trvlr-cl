813b1802164e32daabbf595e9631394d22459b32
Christian Lampl <christian.lampl@gmx.at>
2021-04-17 11:48:18 -0400

    Add: enums for status and visibility

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -2,4 +2,6 @@ class Segment < ApplicationRecord
   belongs_to :trip
   belongs_to :waypoint_from, class_name: 'Waypoint'
   belongs_to :waypoint_to, class_name: 'Waypoint'
+
+  enum status: %i[open active finished]
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,7 @@
 class Trip < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
+
+  enum status: %i[draft active finished cancelled]
+  enum visibility: %i[private users_only public]
 end
