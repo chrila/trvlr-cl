a74f5939bb0b225f1e8b1787912f15e2236f96dd
Christian Lampl <christian.lampl@gmx.at>
2021-04-17 10:40:07 -0400

    Add: model activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_143757) do
+ActiveRecord::Schema.define(version: 2021_04_17_143931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_04_17_143757) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "activities", "users"
 end
