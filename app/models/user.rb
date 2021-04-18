694f70aaa3b67d32c52f8d9acbb3af11bfa02388
Christian Lampl <christian.lampl@gmx.at>
2021-04-18 14:14:35 -0400

    Add: devise for model User - additional attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,6 @@ class User < ApplicationRecord
   has_many :followers, class_name: 'User', through: :followings_passive, source: :user
   has_many :followings_active, class_name: 'Following', foreign_key: :user_id
   has_many :following, class_name: 'User', through: :followings_active, source: :followed_user
+
+  validates :username, presence: true
 end
