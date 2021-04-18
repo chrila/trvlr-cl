313423cb91fe0f4a19e99a5491a5042b49e9945e
Christian Lampl <christian.lampl@gmx.at>
2021-04-18 12:00:53 -0400

    Add: has-many-through relationship to models Trip and User

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,8 @@ class Trip < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
   has_many :waypoints
+  has_many :trip_users
+  has_many :users, through: :trip_users
 
   enum status: %i[trip_draft trip_active trip_finished trip_cancelled]
   enum visibility: %i[visibility_private visibility_users_only visibility_public]
