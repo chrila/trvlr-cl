8d6bcfc3778e3c9f3e0be577877a2af94a040b76
Christian Lampl <christian.lampl@gmx.at>
2021-04-18 12:38:13 -0400

    Add: has-many-through relationships regarding models User and Following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   has_many :trip_users
   has_many :trips, through: :trip_users
+  has_many :followings_passive, class_name: 'Following', foreign_key: :followed_user_id
+  has_many :followers, class_name: 'User', through: :followings_passive, source: :user
+  has_many :followings_active, class_name: 'Following', foreign_key: :user_id
+  has_many :following, class_name: 'User', through: :followings_active, source: :followed_user
 end
