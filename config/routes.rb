9eacd650e660c7ead0bf3f2628605dd9c4beacbc
Christian Lampl <christian.lampl@gmx.at>
2021-04-18 15:43:46 -0400

    Add: bootstrap-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+
+  root 'home#index'
 end
