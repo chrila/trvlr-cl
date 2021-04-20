290554e8abdfcacbebeb97bfd2dbd2bb2bb1b77d
Christian Lampl <christian.lampl@gmx.at>
2021-04-20 18:53:54 -0400

    Add: counter cache for Comment -> MediaItem

---
+++ db/migrate/20210420225003_add_comments_count_to_media_item.rb
@@ -0,0 +1,18 @@
+class AddCommentsCountToMediaItem < ActiveRecord::Migration[6.1]
+  def change
+    add_column :media_items, :comments_count, :integer, default: 0, null: false
+
+    reversible do |dir|
+      dir.up { data }
+    end
+  end
+
+  def data
+    execute <<-SQL.squish
+      UPDATE media_items SET comments_count = (
+        SELECT COUNT(1)
+          FROM comments
+         WHERE commentable_type = 'MediaItem' AND commentable_id = media_items.id)
+    SQL
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_224710) do
+ActiveRecord::Schema.define(version: 2021_04_20_225003) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_04_20_224710) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "likes_count", default: 0, null: false
+    t.integer "comments_count", default: 0, null: false
     t.index ["post_id"], name: "index_media_items_on_post_id"
   end
 
