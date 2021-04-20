90673d196565fc22eb1e076770fe55fc44e93f22
Christian Lampl <christian.lampl@gmx.at>
2021-04-20 18:44:21 -0400

    Add: counter cache for Like -> Activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,6 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
+
+  attr_readonly :likes_count
 end
---
+++ db/migrate/20210420223838_add_likes_count_to_activity.rb
@@ -0,0 +1,18 @@
+class AddLikesCountToActivity < ActiveRecord::Migration[6.1]
+  def change
+    add_column :activities, :likes_count, :integer, default: 0, null: false
+
+    reversible do |dir|
+      dir.up { data }
+    end
+  end
+
+  def data
+    execute <<-SQL.squish
+      UPDATE activities SET likes_count = (
+        SELECT COUNT(1)
+          FROM likes
+         WHERE likeable_type = 'Activity' AND likeable_id = activities.id)
+    SQL
+  end
+end
