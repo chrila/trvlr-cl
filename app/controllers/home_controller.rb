92d6f682612b266cc4222cc293541731d2369ad7
Christian Lampl <christian.lampl@gmx.at>
2021-04-19 22:17:23 -0400

    Add: controller for activities

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    redirect_to activities_path if signed_in?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'home/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+
+  get 'home/index', as: 'home'
+  get 'activities/index', as: 'activities'
 
   root 'home#index'
 end
