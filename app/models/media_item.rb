a23bff6b2b5aa427a466dbed5a08e6012241f51b
Christian Lampl <christian.lampl@gmx.at>
2021-04-20 18:34:47 -0400

    Add: counter cache for Like -> MediaItem

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -2,4 +2,6 @@ class MediaItem < ApplicationRecord
   belongs_to :post
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
+
+  attr_readonly :likes_count
 end
---
+++ db/migrate/20210420223159_add_likes_count_to_media_item.rb
@@ -0,0 +1,18 @@
+class AddLikesCountToMediaItem < ActiveRecord::Migration[6.1]
+  def change
+    add_column :media_items, :likes_count, :integer, default: 0, null: false
+
+    reversible do |dir|
+      dir.up { data }
+    end
+  end
+
+  def data
+    execute <<-SQL.squish
+      UPDATE media_items SET likes_count = (
+        SELECT COUNT(1)
+          FROM likes
+         WHERE likeable_type = 'MediaItem' AND likeable_id = media_items.id)
+    SQL
+  end
+end
