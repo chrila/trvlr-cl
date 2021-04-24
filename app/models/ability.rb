9163bf41f3b3380503a5d0989079872f9b5dffd6
Christian Lampl <christian.lampl@gmx.at>
2021-04-24 01:26:45 -0400

    Fix: ability for like/dislike on comment and activity

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -27,27 +27,24 @@ class Ability
     # users can see their summary page
     can :read, :summary
 
-
     # User profiles
     # users can see other users' profile pages
     can :read, User
 
     # Comments
-    # users can read and create comments, but only delete their own comments
+    # users can read, create, like and dislike comments, but only delete their own comments
     can :destroy, Comment, user_id: user.id
     can :read, Comment
-    can %i[new_activity new_media_item new_post new_trip], Comment
+    can %i[new_activity new_media_item new_post new_trip create like dislike], Comment
 
     # Activities
-    # users can read all activities
-    can :read, Activity
-    can :like, :activity
-    can :dislike, :activity
+    # users can read, like and dislike all activities
+    can %i[read like dislike], Activity
 
     # Trips
     # users can see trips that are set to 'visibility_users'
     can :read, Trip, visibility: :visibility_users
-    
+
     # private trips can only be read by participants
     can :read, Trip do |t|
       t.visibility == :visibility_private && user_ids.include?(user.id)
