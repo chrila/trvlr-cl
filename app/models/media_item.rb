50bddf748b2698a8d74442bea729797306a3f822
Christian Lampl <christian.lampl@gmx.at>
2021-04-23 22:24:39 -0400

    Add: trip relationship to Post and MediaItem

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -1,12 +1,13 @@
 class MediaItem < ApplicationRecord
   belongs_to :waypoint
   belongs_to :user
+  has_one :trip, through: :waypoint
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
 
   attr_readonly :likes_count
   attr_readonly :comments_count
 
-  scope :for_trip, ->(trip) { where(waypoint: trip.waypoints) }
+  scope :for_trip, ->(trip) { where(trip: trip) }
   scope :for_waypoint, ->(waypoint) { where(waypoint: waypoint) }
 end
