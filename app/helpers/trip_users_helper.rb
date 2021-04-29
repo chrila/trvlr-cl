0d9e5f5d9991400ddb4e1ef3ef68766ee8b1a901
Christian Lampl <christian.lampl@gmx.at>
2021-04-29 16:45:02 -0400

    Fix: optimized layout of TripUser#index view

--- app/helpers/trip_users_helper.rb
+++ app/helpers/trip_users_helper.rb
@@ -2,4 +2,13 @@ module TripUsersHelper
   def trip_user_roles_list
     TripUser.roles.keys.collect { |r| [r.split('_').last.humanize, r] }
   end
+
+  def bg_class_for_role(trip_user)
+    badge_classes = {
+      role_administrator: 'bg-primary',
+      role_editor: 'bg-success',
+      role_participant: 'bg-secondary'
+    }
+    badge_classes[trip_user.role.to_sym]
+  end
 end
