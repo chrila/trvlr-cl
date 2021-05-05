c00f34769bd3a8e2bf8a024b9d9f957a4f8a5d4b
Christian Lampl <christian.lampl@gmx.at>
2021-05-04 21:03:56 -0400

    Add: various improvements in waypoint views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def beautify_classname(klass)
     klass.class.to_s.underscore.gsub('_', ' ')
   end
+
+  def truncate_string(str, len = 30)
+    str.length > len ? "#{str[0, len]}..." : str
+  end
 end
