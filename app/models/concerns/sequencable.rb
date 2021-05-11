0aa61b2eaddb85d31a0c6fd33e6b82061505753d
Christian Lampl <christian.lampl@gmx.at>
2021-05-10 23:18:50 -0400

    Add: moved sequencing logic from model to concern

---
+++ app/models/concerns/sequencable.rb
@@ -0,0 +1,52 @@
+module Sequencable
+  extend ActiveSupport::Concern
+
+  def first?
+    !data_collection.find_by(sequence: sequence - 1).present?
+  end
+
+  def last?
+    !data_collection.find_by(sequence: sequence + 1).present?
+  end
+
+  def increase_sequence
+    update_sequence(sequence + 1)
+  end
+
+  def decrease_sequence
+    update_sequence(sequence - 1)
+  end
+
+  def update_sequence(new_seq)
+    do_sequence_update(sequence, new_seq) unless new_seq < 1 || new_seq > data_collection.maximum(:sequence)
+  end
+
+  private
+
+  def do_sequence_update(m, n)
+    # cache sequence and ids, because the sequences will be updated in the DB as we go
+    seq_ids = data_collection.pluck(:sequence, :id).to_h
+    upper = [m, n].max
+    lower = [m, n].min
+    delta = upper - lower
+
+    for i in (delta + 1).downto 1 do
+      x = upper - (i % (delta + 1))
+      y = upper - i + 1
+
+      if m > n
+        set_sequence(seq_ids[x], y)
+      else
+        set_sequence(seq_ids[y], x)
+      end
+    end
+  end
+
+  def set_sequence(id, seq_new)
+    data_collection.find(id).update(sequence: seq_new)
+  end
+
+  def init_sequence
+    update(sequence: (data_collection.maximum(:sequence) || 0) + 1)
+  end
+end
--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,4 +1,6 @@
 class Segment < ApplicationRecord
+  include Sequencable
+
   belongs_to :trip
   belongs_to :waypoint_from, class_name: 'Waypoint'
   belongs_to :waypoint_to, class_name: 'Waypoint'
@@ -11,58 +13,13 @@ class Segment < ApplicationRecord
     segment_finished.where('segments.time_to between ? and ?', date_from, date_to)
   }
 
-  def increase_sequence
-    update_sequence(sequence + 1)
-  end
-
-  def decrease_sequence
-    update_sequence(sequence - 1)
-  end
-
-  def update_sequence(new_seq)
-    return if new_seq < 1 || new_seq > trip.segments.maximum(:sequence)
-
-    do_sequence_update(sequence, new_seq)
-  end
-
-  def first?
-    !trip.segments.find_by(sequence: sequence - 1).present?
-  end
-
-  def last?
-    !trip.segments.find_by(sequence: sequence + 1).present?
-  end
-
   def status_string
     status.split('_').last.humanize
   end
 
   private
 
-  def do_sequence_update(m, n)
-    # cache sequence and ids, because the sequences will be updated in the DB as we go
-    seq_ids = trip.segments.pluck(:sequence, :id).to_h
-    upper = [m, n].max
-    lower = [m, n].min
-    delta = upper - lower
-
-    for i in (delta + 1).downto 1 do
-      x = upper - (i % (delta + 1))
-      y = upper - i + 1
-
-      if m > n
-        set_sequence(seq_ids[x], y)
-      else
-        set_sequence(seq_ids[y], x)
-      end
-    end
-  end
-
-  def set_sequence(id, seq_new)
-    trip.segments.find(id).update(sequence: seq_new)
-  end
-
-  def init_sequence
-    update(sequence: (trip.segments.maximum(:sequence) || 0) + 1)
+  def data_collection
+    trip.segments
   end
 end
