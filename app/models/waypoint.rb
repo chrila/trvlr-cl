ececf0353a42ae255bbc606862b98948e7a9c7a6
Christian Lampl <christian.lampl@gmx.at>
2021-05-10 23:23:01 -0400

    Add: use new concern (sequencable) in waypoint model

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -1,34 +1,12 @@
 class Waypoint < ApplicationRecord
+  include Sequencable
+
   belongs_to :trip
   has_many :segments_starting, class_name: 'Segment', foreign_key: 'waypoint_from_id', dependent: :destroy
   has_many :segments_ending, class_name: 'Segment', foreign_key: 'waypoint_to_id', dependent: :destroy
   has_many :media_items, dependent: :destroy
 
-  after_create :set_sequence
-
-  def increase_sequence
-    wp_after = trip.waypoints.find_by(sequence: sequence + 1)
-    return unless wp_after
-
-    wp_after.update(sequence: wp_after.sequence - 1)
-    update(sequence: sequence + 1)
-  end
-
-  def decrease_sequence
-    return if sequence == 1
-
-    wp_before = trip.waypoints.find_by(sequence: sequence - 1)
-    wp_before.update(sequence: wp_before.sequence + 1)
-    update(sequence: sequence - 1)
-  end
-
-  def first?
-    !trip.waypoints.find_by(sequence: sequence - 1).present?
-  end
-
-  def last?
-    !trip.waypoints.find_by(sequence: sequence + 1).present?
-  end
+  after_create :init_sequence
 
   def lat_long_str
     "#{latitude}, #{longitude}"
@@ -40,7 +18,7 @@ class Waypoint < ApplicationRecord
 
   private
 
-  def set_sequence
-    update(sequence: (trip.waypoints.maximum(:sequence) || 0) + 1)
+  def data_collection
+    trip.waypoints
   end
 end
