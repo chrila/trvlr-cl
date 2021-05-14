0b51e701521a1f85a7c6c251236668202a353ee2
Christian Lampl <christian.lampl@gmx.at>
2021-05-13 23:03:10 -0400

    Add: use uniform layout for TripUser views + added validations

--- app/models/trip_user.rb
+++ app/models/trip_user.rb
@@ -4,6 +4,9 @@ class TripUser < ApplicationRecord
 
   enum role: %i[role_participant role_editor role_administrator]
 
+  validates :user, presence: true
+  validates :role, presence: true, inclusion: { in: TripUser.roles, message: 'is invalid' }
+
   def role_string
     role.split('_').last.humanize
   end
