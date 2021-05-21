45846d21e980a34d60535949167f455d94d075a1
Christian Lampl <christian.lampl@gmx.at>
2021-05-21 16:33:24 -0400

    Add: use ctrl+scroll (desktop), two fingers (mobile) to move/zoom map

--- app/javascript/js/leaflet.js
+++ app/javascript/js/leaflet.js
@@ -1,2 +1,8 @@
 import L from 'leaflet';
+import GestureHandling from "leaflet-gesture-handling";
+
+import "leaflet/dist/leaflet.css";
+import "leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
 import 'leaflet-defaulticon-compatibility';
+
+L.Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
