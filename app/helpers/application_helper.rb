db69b5fe931c36ceb599955909f8273c1d4888ba
Christian Lampl <christian.lampl@gmx.at>
2021-06-14 21:45:52 -0400

    Fix: use unique element ids for like buttons, links and counters

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
     klass.class.to_s.underscore.gsub('_', ' ')
   end
 
+  def dash_classname(klass)
+    klass.class.to_s.underscore.gsub('_', '-')
+  end
+
   def truncate_string(str, len = 30)
     str.length > len ? "#{str[0, len]}..." : str
   end
@@ -17,11 +21,15 @@ module ApplicationHelper
     send("#{likeable.class.to_s.underscore}_dislike_path", likeable)
   end
 
+  def element_id(type, object)
+    "#{dash_classname(object)}-#{type}-#{object.id}"
+  end
+
   def comments_path(commentable)
     "#{url_for(commentable)}#comments"
   end
 
   def icon_thumbs_up(likeable, filled)
-    "<i id='like-button-#{likeable.id}' class='bi bi-hand-thumbs-up#{'-fill' if filled}'></i>"
+    "<i id='#{element_id('like-button', likeable)}' class='bi bi-hand-thumbs-up#{'-fill' if filled}'></i>"
   end
 end
